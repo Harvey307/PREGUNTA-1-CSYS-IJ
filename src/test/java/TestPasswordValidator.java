8e95df12652e4eebaafb260d88e182eca0316951
Erick <erick.agreda@usil.pe>
2025-11-24 22:46:02 -0500

    coberta nueva

--- src/test/java/TestPasswordValidator.java
+++ src/test/java/TestPasswordValidator.java
@@ -42,12 +42,39 @@ public class TestPasswordValidator {
     @Test
     public void testPasswordConEspacios() {
         // No debe tener espacios
-        assertFalse(PasswordValidator.isValid(" 23456789"));
+        assertFalse(PasswordValidator.isValid(" 23456789 "));
     }
 
     @Test
     public void testPasswordSoloLetras() {
 
         assertFalse(PasswordValidator.isValid("abcdefghi"));
+    }
+
+    //test nuevos para completar las ramas logicas
+
+    @Test
+    public void testLongitudMuyLarga() {
+
+        String larga = "A".repeat(65) + "1!";
+        assertFalse(PasswordValidator.isValid(larga));
+    }
+    @Test
+    public void testFaltaNumero() {
+
+        assertFalse(PasswordValidator.isValid("Abcdefg!"));
+    }
+
+    @Test
+    public void testFaltaEspecial() {
+
+        assertFalse(PasswordValidator.isValid("Abcdefg1"));
+    }
+
+
+    @Test
+    public void testFaltaMinuscula() {
+
+        assertFalse(PasswordValidator.isValid("ABCDEFG1!"));
     }
 }
